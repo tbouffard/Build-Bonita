f57583e51afacb1ee4fbb1f55867a6b65f2d81d7
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2019-10-30 16:23:13 +0100

    refactor(studio): remove toolchains.xml configuration file need
    JDK11 is only needed when we want to run tests with this Java version and
    we do not run tests here. So configure the Studio build to avoid the need
    of toolchains.xml file

--- build-script.sh
+++ build-script.sh
@@ -445,7 +445,7 @@ else
 fi
 
 if [[ "${BONITA_BUILD_STUDIO_SKIP}" == "false" ]]; then
-    build_maven_wrapper_verify_skiptest_with_profile bonita-studio default,generate,all-in-one
+    build_maven_wrapper_verify_skiptest_with_profile bonita-studio default,generate,all-in-one,!jdk11-tests
 else
     echo "Skipping the Studio build"
 fi
